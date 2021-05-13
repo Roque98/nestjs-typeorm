ebf2ced6cc8eebbfdb56b25d8b3bc4e3b9d9a8da
Roque Ayala Angel David <ingRoque98@gmail.com>
2021-05-12 21:28:39 -0500

    Creando la relacion muchos a muchos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Brand } from "./brand.entity";
+import { Category } from "./category.entity";
 
 @Entity()
 export class Product {
@@ -35,4 +36,7 @@ export class Product {
 
   @ManyToOne(()=>Brand, (brand)=>brand.products)
   brand: Brand;
+
+  @ManyToMany(()=>Category, (category)=>category.products)
+  categories: Category[];
 }
