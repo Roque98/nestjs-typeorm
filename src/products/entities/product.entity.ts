678cb56cd6129335e384121dad9aca875e55a2bb
Roque Ayala Angel David <ingRoque98@gmail.com>
2021-05-12 20:22:56 -0500

    Creando la relacion uno a muchos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Brand } from "./brand.entity";
 
 @Entity()
 export class Product {
@@ -31,4 +32,7 @@ export class Product {
     default: () => 'CURRENT_TIMESTAMP',
   })
   updateAt: Date;
+
+  @ManyToOne(()=>Brand, (brand)=>brand.products)
+  brand: Brand;
 }
